d554558329c1edfa721b3dc58b1aca4f7b49077a
Jarrod Placide-Raymond <royaltyclubvp@royalty-club.com>
2015-07-06 00:51:38 -0400

    Student Registration Complete

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -5,15 +5,22 @@ $(document).ready(function() {
             var clicked_panel = $(this).attr('id');
             if (current_panel != clicked_panel) {
                 var height = $(window).height();
-                var width = $(window).height();
-                $('#image-container').css({'height': height, 'width': width});
+                var width = $(window).width();
+                $('#background-container').css({'height': height, 'width': width});
                 $('#' + current_panel + '-image, #' + current_panel + '-content').fadeOut(1500);
                 $('#' + clicked_panel + '-image, #' + clicked_panel + '-content').fadeIn(1500);
+                if (clicked_panel != 'about-springboard') {
+                    $('.topbar .link').addClass('black-bg-image');
+                } else {
+                    $('.topbar .link').removeClass('black-bg-image');
+                }
+                $('#' + current_panel).removeClass('selected');
+                $('#' + clicked_panel).addClass('selected');
                 current_panel = clicked_panel;
             }
         });
+        $(window).resize(function(){
+            window.location.href = window.location.href;
+        });
     }
-    $(window).resize(function(){
-        window.location.href = window.location.href;
-    })
 });
