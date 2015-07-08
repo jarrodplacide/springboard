4f6af59507416732a65de32b7579f7eb1a3ddf9e
Jarrod Placide-Raymond <royaltyclubvp@royalty-club.com>
2015-07-08 13:41:30 -0400

    Added Hostname to Production File

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { host: ENV['HOSTNAME'] }
+
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
