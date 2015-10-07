d7e38fb4113eef0897a5a5c59cd99e2e0fbb8db9
Jarrod Placide-Raymond <royaltyclubvp@royalty-club.com>
2015-10-07 08:37:17 -0400

    Change AWS Keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-      :aws_access_key_id        => "AKIAISR63OJ6IPLB5X6A",
-      :aws_secret_access_key    => "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",
+      :aws_access_key_id        => "AKIAJBZ2S2DJTATTUCRA",
+      :aws_secret_access_key    => "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",
       :provider                 => "AWS",
       :host                     => "s3.amazonaws.com",
       :endpoint                 => "https://s3.amazonaws.com",
       :region                   => "us-east-1"
   }
-  config.fog_directory = 'springboard-content'
+  config.fog_directory = 'springboard-production'
 end
